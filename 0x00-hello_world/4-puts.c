e54412cae6a035a272e50c8c8192397547c7f75d
George Maubane <gbmaubane@gmail.com>
2023-07-13 08:48:07 -0700

    alx low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
 *main - using puts
-*Return: Always 0 (success)
+*Return: Always 0 (Success)
 */
-int main(void)
-{   puts("\"Programming is like building a multilingual puzzle\"");
-  return 0;
+int main(void) {
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
