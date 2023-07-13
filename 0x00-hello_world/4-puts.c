b38f0c55257bbc9c6bcb054a56074d9c89af213b
George Maubane <gbmaubane@gmail.com>
2023-07-13 04:54:50 -0700

    alx low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-int main() { puts("Programming is like building a multilingual puzzle"); return 0; }
+gcc -xc - <<<'int main() { puts("Programming is like building a multilingual puzzle"); return 0; }' -o program && ./program
