68bafa6413810dd9ac0b2240c9523ab70aead7e6
George Maubane <gbmaubane@gmail.com>
2023-07-13 10:36:25 -0700

    alx low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 /**
-*main - using printf
+*main - quotes
 *Return: Always 0 (Success)
 */
 int main(void)
 {
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 write(STDERR_FILENO, message, strlen(message));
 return (1);
