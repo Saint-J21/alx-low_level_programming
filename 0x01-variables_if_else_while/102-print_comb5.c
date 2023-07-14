2684d503907b08552aaffb71ee15d72cf36f2b1b
George Maubane <gbmaubane@gmail.com>
2023-07-14 02:45:44 -0700

    alx low level programming

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - The Alphabet - q and e
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int number1;
+for (number1 = 0; number1 <= 99; number1++)
+{
+int number2;
+for (number2 = number1; number2 <= 99; number2++)
+{
+putchar((number1 / 10) + '0');
+putchar((number1 % 10) + '0');
+putchar(' ');
+putchar((number2 / 10) + '0');
+putchar((number2 % 10) + '0');
+if (number1 != 99 || number2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return 0;
+}
