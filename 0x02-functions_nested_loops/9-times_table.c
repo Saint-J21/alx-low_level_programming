bfcd12c04d2b0651f8616aeec924bfda0c6f7a24
George Maubane <gbmaubane@gmail.com>
2023-07-18 09:56:54 -0700

    alx low level programming

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+int row, column;
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+int result = row * column;
+if (column != 0)
+_putchar(',');
+if (result < 10)
+_putchar(' ');
+_putchar('0' + result);
+}
+_putchar('\n');
+}
+}
