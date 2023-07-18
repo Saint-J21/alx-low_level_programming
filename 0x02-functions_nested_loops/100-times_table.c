ce71143e69f65120a2c3cade432639817cf3a878
George Maubane <gbmaubane@gmail.com>
2023-07-18 10:35:01 -0700

    alx low level programming

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * times_table - prints the n times table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  * @n: the value of n
  */
-void times_table(int n)
+void print_times_table(int n)
 {
 int i, j, result;
 if (n > 15 || n < 0)
