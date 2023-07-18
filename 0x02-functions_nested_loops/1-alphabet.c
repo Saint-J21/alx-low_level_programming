2088943feb775d31d52eb62786bc8aa5dae11db5
George Maubane <gbmaubane@gmail.com>
2023-07-18 03:54:45 -0700

    alx low level programming

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,9 @@
 *main - putchar
 *Return: Always 0.
 */
-void print_alphabet(void)
+int main(void)
 {
-int i;
-for (i = 0; i < 26; i++)
+print_alphabet(int i; for (i = 0; i < 26; i++))
 {
 _putchar('a' + i);
 }
