6c1713075307df993cfff2a6116d25a62ac26db6
George Maubane <gbmaubane@gmail.com>
2023-07-20 11:29:35 -0700

    alx low level programming

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include "main.h"
 
 /**
  * largest_prime_factor - Finds the largest prime factor of a given number
@@ -10,11 +11,15 @@
 long largest_prime_factor(long num)
 {
 long largest_prime = -1;
+
+// Divide by 2 to remove all even factors
 while (num % 2 == 0)
 {
 largest_prime = 2;
 num /= 2;
 }
+
+// Check for odd prime factors
 for (long i = 3; i <= sqrt(num); i += 2)
 {
 while (num % i == 0)
@@ -23,14 +28,20 @@ largest_prime = i;
 num /= i;
 }
 }
+
+// If the remaining number is a prime greater than 2
 if (num > 2)
 largest_prime = num;
+
 return (largest_prime);
 }
+
 int main(void)
 {
 long num = 612852475143;
 long result = largest_prime_factor(num);
-printf("%ld\n", result);
+
+print_number(result);
+_putchar('\n');
 return (0);
 }
