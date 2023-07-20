2c38b3838b4c808bdfe5c45725e49c9ba9f3023a
George Maubane <gbmaubane@gmail.com>
2023-07-20 09:28:26 -0700

    alx low level programming

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,14 @@
 #include "main.h"
 
 /**
-* main - prints the numbers from 1 to 100
+* main - Entry point for the Fizz-Buzz program
 *
-* Result: Always 0.
+* This program prints the numbers from 1 to 100, each followed by a space. For
+* numbers divisible by 3, it prints "Fizz" instead of the number. For numbers
+* divisible by 5, it prints "Buzz" instead of the number. For numbers that are
+* multiples of both 3 and 5, it prints "FizzBuzz".
+*
+* Result: Always 0 (success)
 */
 
 int main(void)
