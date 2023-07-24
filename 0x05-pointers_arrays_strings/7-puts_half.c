f143595a2874e59e3f3abc6a225f4ff51f23068b
George Maubane <gbmaubane@gmail.com>
2023-07-24 03:35:24 -0700

    alx low level programming

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of string.
+* @s: pointer
+*
+* Return: The length of the string
+*/
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+* puts_half - prints the second half
+* @str: pointer
+*/
+
+void puts_half(char *str)
+{
+	int length = _strlen(str);
+	int start_index;
+
+if (length % 2 == 0)
+{
+	start_index = length / 2;
+}
+else
+{
+	start_index = (length - 1) / 2;
+}
+
+while (str[start_index] != '\0')
+{
+	_putchar(str[start_index]);
+	start_index++;
+}
+	_putchar('\n')
+}
