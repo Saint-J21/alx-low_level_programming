7bf74e59a1d98f66144bab7121685a63e39824a6
George Maubane <gbmaubane@gmail.com>
2023-07-24 05:30:39 -0700

    alx low level programming

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,22 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* print_array - Prints n elements
-* @a: Pointer to the array
-* @n: Number of elements
-*/
-
+ * print_array - Prints n elements of an array of integers.
+ * @a: Pointer to the array of integers.
+ * @n: Number of elements to be printed.
+ */
 void print_array(int *a, int n)
 {
-	if (a == NULL || n <= 0)
-		return;
+if (a == NULL || n <= 0)
+return;
 
-	for (int i = 0; i < n; i++)
-	{
-		_putchar('0' + a[i]);
+for (int i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+printf(", ");
+}
 
-	if (i < n - 1)
-		_puts(", ");
-	}
-_putchar('\n');
+printf("\n");
+}
+
+int main(void)
+{
+int arr[] = {1, 2, 3, 4, 5};
+int n = sizeof(arr) / sizeof(arr[0]);
+
+print_array(arr, n); // Output: 1, 2, 3, 4, 5
+
+return (0);
 }
