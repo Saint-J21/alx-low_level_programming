d3fa76e23bcf155fff04fd1b2c5769a4e74b5410
George Maubane <gbmaubane@gmail.com>
2023-07-24 05:05:14 -0700

    alx low level programming

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,12 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if (str[i] >= 32 && str[i] <= 126)
+			_putchar(str[i]);
 		i += 2;
 
 		if (str[i] == '\0')
 			break;
 	}
-
 	_putchar('\n');
 }
