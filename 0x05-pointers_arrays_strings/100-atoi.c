0f113afbafaf7db9ab06933a2210d614cf8cd66f
George Maubane <gbmaubane@gmail.com>
2023-07-24 10:34:36 -0700

    alx low level programming

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _atoi - converts a string to an integer.
+* @s: pointer to the string.
+*
+* Return: the converted integer value.
+*/
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+	else
+	{
+		break;
+	}
+	i++;
+}
+return (result * sign);
+}
