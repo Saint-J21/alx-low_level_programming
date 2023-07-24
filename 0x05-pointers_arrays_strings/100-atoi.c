8b2f9a0abf79d6683009cdd53cc82e5cfecd7d71
George Maubane <gbmaubane@gmail.com>
2023-07-24 10:45:26 -0700

    alx low level programming

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,13 +24,22 @@ int _atoi(char *s)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
+			
+			if (result > (INT_MAX - (s[i] - '0')) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+
 			result = result * 10 + (s[i] - '0');
 		}
-	else
-	{
-		break;
+		else
+		{
+			break;
+		}
+		i++;
 	}
-	i++;
-}
-return (result * sign);
+	return (result * sign);
 }
