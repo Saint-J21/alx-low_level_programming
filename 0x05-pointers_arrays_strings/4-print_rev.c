e00c3e38ee18e69325246af05a5ac8c47ed5725f
George Maubane <gbmaubane@gmail.com>
2023-07-24 02:54:17 -0700

    alx low level programming

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _strlen - Returns length of string.
+* @s: Pointer to the string.
+*
+* Return: The length of string
+*/
+
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+* print_rev - Prints a string in reverse.
+* @s: Pointer to the string.
+*/
+
+void print_rev(char *s)
+{
+	int length = _strlen(s);
+	int i;
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
