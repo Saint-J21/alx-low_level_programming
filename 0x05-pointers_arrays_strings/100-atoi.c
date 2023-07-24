1aad92889b3e68b87642fbf5f83820cde80add2d
George Maubane <gbmaubane@gmail.com>
2023-07-24 11:08:32 -0700

    alx low level programming

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,37 +10,52 @@
 
 int _atoi(char *s)
 {
-	int sign = -1;
-	int result = 0;
-	int i = 0;
+int sign = 1;
+int result = 0;
+int i = 0;
+int is_negative = 0;
 
-	while (s[i] == '-' || s[i] == '+')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
-	}
+while (s[i] == '-' || s[i] == '+')
+{
+if (s[i] == '-')
+sign *= -1;
+i++;
+}
 
-	while (s[i] != '\0')
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			
-			if (result > (INT_MAX - (s[i] - '0')) / 10)
-			{
-				if (sign == 1)
-					return (INT_MAX);
-				else
-					return (INT_MIN);
-			}
+if (s[i] == '\0')
+return (0);
 
-			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-			break;
-		}
-		i++;
-	}
-	return (result * sign);
+if (s[i] == '0')
+{
+result = 0;
+return (result);
+}
+
+while (s[i] != '\0')
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+if (result > INT_MAX / 10)
+{
+if (sign == 1)
+return (INT_MAX);
+else
+return (INT_MIN);
+}
+result = result * 10 + (s[i] - '0');
+}
+else
+{
+break;
+}
+i++;
+}
+
+if (sign == -1)
+is_negative = 1;
+
+if (is_negative)
+return (-result);
+else
+return (result);
 }
