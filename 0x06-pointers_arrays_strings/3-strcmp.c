c29604cc81454ce041426b27e32d8acb9306938e
George Maubane <gbmaubane@gmail.com>
2023-07-26 05:03:04 -0700

    alx low level programming

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares tow strings
+ *
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * This function compare the two strings @s1 and @s2. It returns an
+ * integer value less than, equal to, or greater than 0 if @s1 is found
+ * to be less than, equal to, or greater than @s2, respectively.
+ *
+ * Return: an integer value less than, equal to, or grreater than 0.
+ */
+
+int _strcmp(const char *s1, const char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
