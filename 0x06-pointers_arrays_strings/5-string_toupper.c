cdded6d14f589e81791f479fedf6f16417cc7e9f
George Maubane <gbmaubane@gmail.com>
2023-07-27 00:39:19 -0700

    alx low level programming

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * @str: the input string.
+ *
+ * Return: a pointer to the resulting string.
+ */
+
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - ('a' - 'A');
+		}
+		ptr++;
+	}
+
+	return (str);
+}
