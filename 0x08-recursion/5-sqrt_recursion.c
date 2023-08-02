02685fd71bcd2bf0fd8569316b31fdb182537176
George Maubane <gbmaubane@gmail.com>
2023-08-02 08:10:56 -0700

    alx low level programming

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -26,3 +26,22 @@ int find_sqrt(int n, int start, int end)
 	}
 	return (-1);
 }
+
+/**
+ * _sqrt_recursion - returns 
+ *
+ * @n: the number to be calculated.
+ *
+ * Description:
+ * This function recursively calculates.
+ *
+ * Return: the natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (find_sqrt(n, 0, n))
+}
