c40cde1c41d1bde4077e64a73aefcec6d1a8f4a3
George Maubane <gbmaubane@gmail.com>
2023-08-02 08:54:34 -0700

    alx low level prgramming

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+* _strlen - Returns the length of a string.
+*
+* @s: Pointer to the string whose length is to be determined.
+*
+* Return: The length of the string.
+*/
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
+
+/**
+* is_palindrome_recursive - Helper function to check if a string is a palindrome
+* 
+* @s: Pointer to the string to be checked.
+* @start: The starting index of the string.
+* @end: The ending index of the string.
+*
+* Return: 1 if the string is a palindrome, otherwise 0.
+*/
+
+int is_palindrome_recursive(char *s, int start, int end)
+{
+if (start >= end)
+return (1);
+
+if (s[start] != s[end])
+return (0);
+
+return (is_palindrome_recursive(s, start + 1, end - 1));
+}
+
+/**
+* is_palindrome - Checks if a string is a palindrome.
+*
+* @s: Pointer to the string to be checked.
+*
+* Description:
+* This function checks if the input string 's' is a palindrome
+* by calling a recursive helper function 'is_palindrome_recursive'.
+*
+* Return: 1, otherwise 0.
+*/
+int is_palindrome(char *s)
+{
+    int len = _strlen(s);
+
+    if (len <= 1)
+        return (1);
+return (is_palindrome_recursive(s, 0, len - 1));
+}
