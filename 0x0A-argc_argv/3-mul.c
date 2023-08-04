de163add899389c01f2368ebbdc548c555f51508
George Maubane <gbmaubane@gmail.com>
2023-08-04 10:01:10 -0700

    alx low level programming

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+int i, n;
+n = 1;
+
+if (argc < 2)
+{
+printf("error\n");
+return (1);
+}
+for (i = 1; i < argc; i++)
+{
+n = atoi (argv[i]) * n;
+}
+printf("%d\n", n);
+return (0);
+}
