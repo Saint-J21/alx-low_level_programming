3b1dd103224f5bbe4d903a49a79e79c6c67b1d70
George Maubane <gbmaubane@gmail.com>
2023-08-04 16:01:03 -0700

    alx low level programming

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+int main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
+
 {
-	char* program_name = argv[0];
+	char *program_name = argv[0];
 
 	printf("%s\n", program_name);
 
