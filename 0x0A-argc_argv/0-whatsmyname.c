a04975c8e6b49a5280d1e3f0fccd22425814d068
George Maubane <gbmaubane@gmail.com>
2023-08-04 08:58:43 -0700

    alx low level programming

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+	std::string program_name = argv[0];
+
+	std::cout << "Program name: " << program_name << std::endl;
+
+	return (0);
+}
