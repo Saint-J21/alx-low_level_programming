f03f79e62b2e9691c98c3b9bcf6e2f64123e2c89
George Maubane <gbmaubane@gmail.com>
2023-08-04 16:52:41 -0700

    alx low level programming

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - entry
+ * @argc: the number
+ * @argv: an array
+ *
+ * Description:
+ * - The main function calculates the number of command-line arguments passed
+ * to the program (excluding the program name itself) and prints the result.
+ * The unused __attribute__ is used to inform the compiler that the argc and
+ * argv parameters are intentionally left unused in this program.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int __attribute__((unused)) argc,
+		char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 
