b522c2aa4ee1971d2fcf70ab111b4c0c03bd0130
George Maubane <gbmaubane@gmail.com>
2023-08-04 16:39:15 -0700

    alx low level programming

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
+/**
+ * main - entry
+ * @argc: the number
+ * @argv: an array
+ * @__attribute__: unused
+ *
+ * Description:
+ * - main function: Prints the name of the progra
+ * - __attribute__: GCC ext
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int __attribute__((unused)) argc,
+	char __attribute__((unused)) *argv[])
 
 {
 	char *program_name = argv[0];
