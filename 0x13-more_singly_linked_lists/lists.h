bc7ada02c72d876b453118a92615c79ce9f8a149
Saint-J21 <gbmaubane@gmail.com>
2023-08-28 05:01:11 -0700

    alx low level programming

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef LIST_H
 #define LIST_H
 
+typedef struct listint_t
+{
+	int n;
+	struct listint_t *next;
+}
+listint_t;
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
