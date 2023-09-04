0578f5d2f06f99475e17f753e3c9e06a6e7db83a
Saint-J21 <gbmaubane@gmail.com>
2023-09-04 10:01:08 -0700

    lx low level programming

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,29 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @filename: The text file to be read.
- * @letters: The number of letters to be read.
- * Return: The actual number of bytes read and printed.
- *         0 when the function fails or the filename is NULL.
+ * read_textfile - STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-	return (0);
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	char *buffer = malloc(sizeof(char) * letters);
-    	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
-
-	ssize_t total_bytes_read = 0;
-	ssize_t bytes_read;
-	ssize_t bytes_written;
-
-	while ((bytes_read = read(fd, buffer, letters)) > 0)
-	{
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-		total_bytes_read += bytes_read;
-	}
-
-	free(buffer);
+	free(buf);
 	close(fd);
-	return (total_bytes_read);
+	return (w);
 }
-
